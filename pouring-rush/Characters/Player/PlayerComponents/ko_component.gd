8fffb06f11f8e85badcf5b5e081f09117bf9da76
ADHdne <aadnevat@hotmail.com>
2026-06-07 08:31:35 +0200

    Update ko_component.gd

--- pouring-rush/Characters/Player/PlayerComponents/ko_component.gd
+++ pouring-rush/Characters/Player/PlayerComponents/ko_component.gd
@@ -4,15 +4,15 @@ class_name KOComponent
 @export var player : Player
 
 
-@export var wall_ko_speed : float = 1200
-@export var ceiling_ko_speed : float = 1000
+@export var wall_ko_speed : float = 1000
+@export var ceiling_ko_speed : float = 800
 
 
 func check_impact():
 	
 	var speed = player.velocity.length()
 	
-	if speed >= ceiling_ko_speed - 300:
+	if speed >= ceiling_ko_speed:
 		KO()
 	
 
