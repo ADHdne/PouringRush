18e27addd61c48ca98fa7a742c00b35f270a86a6
ADHdne <aadnevat@hotmail.com>
2026-06-09 06:59:10 +0200

    made match manager and stuff

---
+++ pouring-rush/MatchLogic/match_manager.gd
@@ -0,0 +1,44 @@
+extends Node
+class_name MatchManager
+
+
+var arena : Arena
+var game_mode : GameMode
+
+var players : Array[Player] = []
+
+var player_scene : PackedScene
+
+func initialize(p_arena : Arena, p_game_mode : GameMode, players_root : Node2D):
+	arena = p_arena
+	game_mode = p_game_mode
+	
+	spawn_players(players_root)
+
+
+func start_match():
+	pass
+
+func spawn_players(players_root):
+	var p1 = player_scene.instansiate()
+	var p2 = player_scene.instansiate()
+	
+	players_root.add_child(p1)
+	players_root.add_child(p2)
+	
+	players = [p1, p2]
+	
+	p1.global_position = arena.get_spawn_point(0).global_position
+	p2.global_position = arena.get_spawn_point(1).global_position
+
+
+func respawn_player(player):
+	# respawns player
+	# this need to reflect what team the player is on
+	player.global_position = arena.get_spawn_point(0).global_position
+	
+	player.reset_for_respawn() # need to make this a function in player
+
+
+func end_match():
+	pass
--- pouring-rush/MatchLogic/match_scene.gd
+++ pouring-rush/MatchLogic/match_scene.gd
@@ -2,10 +2,14 @@ extends Node2D
 class_name MatchScene
 
 
+
+@onready var match_manager: MatchManager = $MatchManager
 @onready var arena_root: Node2D = $ArenaRoot
 @onready var game_mode_root: Node = $GameModeRoot
 @onready var players: Node2D = $Players
 @onready var ui: CanvasLayer = $UI
+
+
 
 func _ready() -> void:
 	# adding the selected arena to the match scene
@@ -16,3 +20,5 @@ func _ready() -> void:
 	
 	var game_mode = GameManager.selected_game_mode.instantiate()
 	game_mode_root.add_child(game_mode)
+	
+	match_manager.initialize(arena, game_mode, players)
