862b0b6d17a6b59e0f24875b185f624a4332f09f
ADHdne <aadnevat@hotmail.com>
2026-06-09 06:35:36 +0200

    added match scene

---
+++ pouring-rush/Globals/game_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+# Game Manager
+
+
+@export var selected_arena : PackedScene
+@export var selected_game_mode : PackedScene
---
+++ pouring-rush/MatchLogic/match_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name MatchScene
+
+
+@onready var arena_root: Node2D = $ArenaRoot
+@onready var game_mode_root: Node = $GameModeRoot
+@onready var players: Node2D = $Players
+@onready var ui: CanvasLayer = $UI
+
+func _ready() -> void:
+	# adding the selected arena to the match scene
+	var arena = GameManager.selected_arena.instantiate()
+	arena_root.add_child(arena)
+	
+	# adding the selected mode to the match scene
+	
+	var game_mode = GameManager.selected_game_mode.instantiate()
+	game_mode_root.add_child(game_mode)
