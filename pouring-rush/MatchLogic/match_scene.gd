25dc32a3d85303393d391887ce7dcd9556959ec6
ADHdne <aadnevat@hotmail.com>
2026-06-11 20:27:06 +0200

    bah

--- pouring-rush/MatchLogic/match_scene.gd
+++ pouring-rush/MatchLogic/match_scene.gd
@@ -3,9 +3,11 @@ class_name MatchScene
 
 
 @onready var match_manager: MatchManager = $MatchManager
+
 @onready var arena_root: Node2D = $ArenaRoot
 @onready var game_mode_root: Node = $GameModeRoot
 @onready var players: Node2D = $Players
+
 @onready var ui: CanvasLayer = $UI
 
 
@@ -13,4 +15,8 @@ class_name MatchScene
 
 
 func _ready() -> void:
+	
+	# both viewports renders the same world
+	
+	
 	match_manager.initialize(GameManager.match_config, arena_root, game_mode_root, players)
