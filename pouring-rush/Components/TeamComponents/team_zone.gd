6b0ffd8a249f5f8032222a3f5959756de2e6420b
ADHdne <aadnevat@hotmail.com>
2026-06-13 08:30:33 +0200

    made gravity for team zone

--- pouring-rush/Components/TeamComponents/team_zone.gd
+++ pouring-rush/Components/TeamComponents/team_zone.gd
@@ -2,6 +2,10 @@ extends Area2D
 class_name TeamZone
 
 
+
+@onready var ray_cast: RayCast2D = $RayCast2D
+
+
 @export var team : Team.type
 
 
@@ -12,6 +16,8 @@ var carrier : Player
 @export var pickup_range : float = 50
 
 @export var grav : float = 1000.0
+@export var grounded : bool = false
+@export var velocity : Vector2
 
 
 func _process(delta: float) -> void:
@@ -20,7 +26,26 @@ func _process(delta: float) -> void:
 	if carrier:
 		global_position = carrier.global_position + carrier_offset
 
+	# setting gravity
+	ground_check()
+	if not grounded and not carrier:
+		velocity.y += grav * delta
+	elif grounded:
+		velocity.y = 0
+	
+	# grav move and slide
+	global_position += velocity * delta
+	
 
+func ground_check():
+	
+	if not ray_cast.is_colliding():
+		grounded = false
+	else:
+		var collider = ray_cast.get_collider()
+		
+		if collider and collider.is_in_group("Ground"):
+			grounded = true
 
 func can_be_picked_up(player : Player) -> bool:
 	if player.team!= team:
