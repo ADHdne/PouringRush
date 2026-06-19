86840e33d699c58c720cbb92dd1680279f531895
ADHdne <aadnevat@hotmail.com>
2026-06-19 19:17:53 +0200

    player UI works

--- pouring-rush/Components/UI/player_icon.gd
+++ pouring-rush/Components/UI/player_icon.gd
@@ -3,19 +3,58 @@ class_name PlayerIcon
 
 
 var player: Player
+var combat_component : CombatComponent
 
 
 @export var portrait : TextureRect
 @onready var percent: Label = $Percent
 
+# the shots cooldowns
+@export var basic_shot : TextureRect
+@export var magazine : Label
 
-func setup(p: Player):
-	player = p
+@export var special_1 : TextureRect
+@export var special_1_bar : ProgressBar
 
-	portrait.texture = player.character_data.portrait
+
+
+@export var special_2 : TextureRect
+@export var special_2_bar : ProgressBar
+
+@export var utility : TextureRect
+@export var utility_bar : ProgressBar
 
 func _process(delta):
 	if player == null:
 		return
 
-	percent.text = str(player.damage_component.percentage)
+	# updating the different labels taxt
+	percent.text = str(player.damage_component.percentage) + "%"
+	magazine.text = str(combat_component.basic_shot_data.current_ammo)
+	
+	# updating progress bars
+	special_1_bar.value = combat_component.special_shot_data.cooldown_remaining
+	special_2_bar.value = combat_component.special_2_data.cooldown_remaining
+	utility_bar.value = combat_component.utility_data.cooldown_remaining
+
+
+func setup(p: Player):
+	player = p
+	combat_component = player.combat_component
+	
+	
+	portrait.texture = player.character_data.portrait
+	set_up_abilities(combat_component)
+
+func set_up_abilities(component : CombatComponent):
+	# setting up all abilities textures
+	basic_shot.texture = component.basic_shot_data.ability_data.projectile_data.texture
+	special_1.texture = component.special_shot_data.ability_data.projectile_data.texture
+	special_2.texture = component.special_2_data.ability_data.projectile_data.texture
+	
+	# setting progressbars max_value
+	special_1_bar.max_value = component.special_shot_data.ability_data.ability_cooldown
+	special_2_bar.max_value = component.special_2_data.ability_data.ability_cooldown
+	utility_bar.max_value = component.utility_data.ability_data.ability_cooldown
+	
+	
