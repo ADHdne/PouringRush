7b7f74ca5ef7746f22a486ce97c08515cc61bf12
ADHdne <aadnevat@hotmail.com>
2026-06-19 08:16:18 +0200

    working on player ui

--- pouring-rush/Characters/Scripts/character.gd
+++ pouring-rush/Characters/Scripts/character.gd
@@ -6,13 +6,3 @@ signal facing_direction_changed(facing_right : bool)
 
 @export var animation_player : AnimationPlayer
 @export var sprite : Sprite2D
-
-var can_flip : bool = true
-
-#func update_facing_direction():
-	#if can_flip == true:
-		#if is_on_floor() or is_on_wall_only():
-			#if direction.x > 0:
-				#sprite.flip_h = false # add facing directions as boolians
-			#elif direction.x < 0:
-				#sprite.flip_h = true
