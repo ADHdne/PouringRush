97faa493ed5370d9ae62d32327a543aec9254a83
ADHdne <aadnevat@hotmail.com>
2026-06-20 09:49:22 +0200

    adde all player actions to player config

--- pouring-rush/Resources/player_config.gd
+++ pouring-rush/Resources/player_config.gd
@@ -7,5 +7,11 @@ var team : Team.type
 var input_id : int
 var player_actions : Array[PlayerActions] = [
 	preload("res://Characters/Player/PlayerResources/PlayerControls/player_1_actions.tres"),
-	preload("res://Characters/Player/PlayerResources/PlayerControls/player_2_actions.tres")
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_2_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_3_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_4_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_5_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_6_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_7_actions.tres"),
+	preload("res://Characters/Player/PlayerResources/PlayerControls/player_8_actions.tres"),
 	]
