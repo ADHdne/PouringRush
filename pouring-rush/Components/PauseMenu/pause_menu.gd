1aaff45de1c05d6d2b5afb90cd8f101bc017e6c2
ADHdne <aadnevat@hotmail.com>
2026-06-21 10:44:47 +0200

    started on adding sound

---
+++ pouring-rush/Components/PauseMenu/pause_menu.gd
@@ -0,0 +1,81 @@
+extends Control
+class_name PauseMenu
+
+
+## references
+@export var animation : AnimationPlayer
+@export var pause_window : VBoxContainer
+@export var htp_window : VBoxContainer
+@export var resume_button : Button
+@export var return_button : Button
+@export var menu_open : bool = false
+
+func _ready() -> void:
+	# grabs first buttons focus
+	resume_button.grab_focus()
+	
+	# removes it so it is not just invisible
+	hide()
+	get_tree().paused = false
+	animation.play("RESET")
+
+
+func resume():
+	resume_button.grab_focus()
+	resume_button.release_focus()
+	get_tree().paused = false
+	animation.play_backwards("Blur")
+	SoundManager.menu_close.play()
+
+func pause():
+	# adds visibility and moves it in front
+	pause_window.visible = true
+	htp_window.visible = false
+	
+	show()
+	move_to_front()
+	
+	resume_button.grab_focus()
+	get_tree().paused = true
+	animation.play("Blur")
+	SoundManager.menu_open.play()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Start") and not get_tree().paused:
+		pause()
+	elif event.is_action_pressed("Start") and get_tree().paused:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_objective_pressed() -> void:
+	SoundManager.confirm.play()
+	
+	pause_window.visible = false
+	htp_window.visible = true
+	return_button.grab_focus()
+
+func _on_return_pressed() -> void:
+	SoundManager.confirm.play()
+	
+	htp_window.visible = false
+	pause_window.visible = true
+	resume_button.grab_focus()
+	
+
+func _on_quit_to_menu_pressed() -> void:
+	resume()
+	
+	SoundManager.main_music.stop()
+	SoundManager.deny.play()
+	
+	await get_tree().create_timer(0.20).timeout
+	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
+
+
+# making sound when button is focused
+func _on_focus_entered() -> void:
+	SoundManager.button.play(0.18)
---
+++ pouring-rush/Globals/sound_manager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+# refrences to children
+@onready var confirm: AudioStreamPlayer = $Confirm
+@onready var deny: AudioStreamPlayer = $Deny
+@onready var press_start: AudioStreamPlayer = $PressStart
+@onready var menu_open: AudioStreamPlayer = $MenuOpen
+@onready var menu_close: AudioStreamPlayer = $MenuClose
+@onready var button: AudioStreamPlayer = $Button
+@onready var enterance: AudioStreamPlayer = $Enterance
+@onready var big_droplet: AudioStreamPlayer = $BigDroplet
+@onready var menu_music: AudioStreamPlayer = $MenuMusic
+@onready var main_music: AudioStreamPlayer = $MainMusic
