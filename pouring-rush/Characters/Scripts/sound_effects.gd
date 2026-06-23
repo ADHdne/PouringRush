f382e8bd3191569f0ebd40fa219044079b0e0f3c
ADHdne <aadnevat@hotmail.com>
2026-06-23 07:05:54 +0200

    working on sound

--- pouring-rush/Characters/Scripts/sound_effects.gd
+++ pouring-rush/Characters/Scripts/sound_effects.gd
@@ -1,2 +1,32 @@
 extends Node2D
 class_name CharacterSoundEffects
+
+
+
+@export var dash_sound : AudioStreamPlayer2D
+@export var hit_sound : AudioStreamPlayer2D
+@export var hit_rock_sound : AudioStreamPlayer2D
+@export var glass_break_sound : AudioStreamPlayer2D
+@export var splatt_sound : AudioStreamPlayer2D
+@export var footstep_sound : AudioStreamPlayer2D
+@export var jumpswoosh_sound : AudioStreamPlayer2D
+
+
+
+
+func run():
+	footstep_sound.pitch_scale = randf_range(0.8, 1.2)
+	footstep_sound.play()
+
+
+func jump():
+	jumpswoosh_sound.pitch_scale = randf_range(0.8, 1.2)
+	jumpswoosh_sound.play()
+
+
+func hit():
+	hit_sound.play()
+	splatt_sound.play()
+
+func dash():
+	dash_sound.play()
