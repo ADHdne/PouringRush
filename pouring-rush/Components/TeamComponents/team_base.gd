b5a5a8cc72aee00f1a457f6c3d5474c9e7bd4286
ADHdne <aadnevat@hotmail.com>
2026-06-24 06:53:30 +0200

    team base also have a visible safezone

--- pouring-rush/Components/TeamComponents/team_base.gd
+++ pouring-rush/Components/TeamComponents/team_base.gd
@@ -1,2 +1,17 @@
 extends SafeZoneArea
 class_name TeamBase
+
+
+@export var area_visual : ColorRect
+@export var collision_shape : CollisionShape2D
+
+func _ready() -> void:
+	setup_size()
+
+
+
+
+func setup_size():
+	
+	area_visual.size = collision_shape.shape.get_rect().size
+	area_visual.position = -collision_shape.shape.size / 2.0
