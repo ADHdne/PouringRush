3b3e9b0baa1270630ffd2bcf3624c0132cb79733
ADHdne <aadnevat@hotmail.com>
2026-06-26 17:00:22 +0200

    added get hit sound

--- pouring-rush/Characters/Player/PlayerComponents/hurtbox_component.gd
+++ pouring-rush/Characters/Player/PlayerComponents/hurtbox_component.gd
@@ -11,6 +11,7 @@ func recieve_hit(hit_data : HitData, player : Player):
 
 	if knockback_component != null:
 		knockback_component.apply_knockback(hit_data)
+		player.sound_effects.hit()
 	if damage_component != null:
 		damage_component.apply_damage(hit_data, player)
 		damage_component.apply_heal(hit_data, player)
