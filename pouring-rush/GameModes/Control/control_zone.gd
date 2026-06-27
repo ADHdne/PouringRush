58caf58a432d46b0d0b4503d7166abc8ecd58ecd
ADHdne <aadnevat@hotmail.com>
2026-06-27 16:12:28 +0200

    started working on control mode

---
+++ pouring-rush/GameModes/Control/control_zone.gd
@@ -0,0 +1,20 @@
+extends Area2D
+class_name ControlZone
+
+
+
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var area_visual: ColorRect = $ColorRect
+
+
+
+func _ready() -> void:
+	setup_size()
+
+
+
+func setup_size():
+	
+	area_visual.size = collision_shape.shape.get_rect().size
+	area_visual.position = -collision_shape.shape.size / 2.0
