d6926e0febe3dd1756ff0ffe1a68a59ed5f0beed
ADHdne <aadnevat@hotmail.com>
2026-06-27 16:30:40 +0200

    main menu is set for choose game mode

--- pouring-rush/main_menu/main_menu.gd
+++ pouring-rush/main_menu/main_menu.gd
@@ -9,6 +9,7 @@ class_name MainMenu
 @export var host_game_menu : VBoxContainer
 @export var character_selection_menu : VBoxContainer
 @export var choose_team_menu : VBoxContainer
+@export var choose_game_mode_menu : VBoxContainer
 
 
 # reference to other children
@@ -28,8 +29,9 @@ var arenas : Array[PackedScene] = [
 ]
 
 var game_modes : Array[PackedScene] = [
-	preload("res://GameModes/stock_mode.tscn"),
-	preload("res://GameModes/Push/push_mode.tscn")
+	preload("res://GameModes/Stock/stock_mode.tscn"),
+	preload("res://GameModes/Push/push_mode.tscn"),
+	preload("res://GameModes/Control/control_mode.tscn")
 	
 ]
 
@@ -42,8 +44,12 @@ var current_menu : VBoxContainer
 var players : Array[PlayerConfig]
 var number_of_players : int = 2
 
+var config : MatchConfig
 
 var active_player : PlayerConfig
+
+var game_mode_selected : int
+
 
 func _ready() -> void:
 	# turns of visibility of the other screens
@@ -55,6 +61,8 @@ func _ready() -> void:
 	number_of_players_label.text = "Number Of Players: 2"
 	
 	SoundManager.menu_music.play()
+	
+	config = MatchConfig.new()
 
 
 
@@ -97,6 +105,9 @@ func update_menu_title(menu : VBoxContainer):
 	match menu.name:
 		"HostGameMenu":
 			menu_title.text = "Player Amount"
+		
+		"ChooseGameMode":
+			menu_title.text = "Choose Game Mode"
 			
 		"ChooseTeamMenu":
 			menu_title.text = "Select Team"
@@ -109,12 +120,14 @@ func update_menu_title(menu : VBoxContainer):
 		
 
 func start_match():
-	var config = MatchConfig.new()
+	
+	
+	config.game_mode = game_modes[game_mode_selected]
 	config.players = players
+	
 	
 	# both arena and game mode should be choosable in this menu in the future
 	config.arena_scene = arenas[0]
-	config.game_mode = game_modes[1]
 	# also setting the game mode as matchable var
 	config.game_mode_type = SelectedMode.Mode.PUSH
 	
@@ -150,13 +163,15 @@ func _on_return_button_pressed() -> void:
 
 func _on_host_game_pressed() -> void:
 	# opening the next menu
-	open_menu(host_game_menu)
+	open_menu(choose_game_mode_menu)
 	
 	SoundManager.confirm.play()
 
 
 func _on_join_game_pressed() -> void:
-	pass # Replace with function body.
+	SoundManager.confirm.play()
+	
+	print("join game pressed")
 
 
 func _on_options_button_pressed() -> void:
@@ -171,11 +186,25 @@ func _on_exit_button_pressed() -> void:
 
 
 
+## Choose Game Mode Menu
+
+func _on_push_mode_pressed() -> void:
+	
+	game_mode_selected = 1
+	
+	open_menu(host_game_menu)
+
+func _on_control_pressed() -> void:
+	
+	game_mode_selected = 2
+	
+	open_menu(host_game_menu)
+
+
+
 ## host game menu
 
 func _on_select_team_buttons_pressed() -> void:
-	# opening the next menu
-	open_menu(choose_team_menu)
 	
 	# starting the new character
 	active_player = PlayerConfig.new()
@@ -186,6 +215,9 @@ func _on_select_team_buttons_pressed() -> void:
 	game_text.text = "Configuring Player " + str(active_player.input_id + 1)
 	
 	SoundManager.confirm.play()
+	
+	# opening the next menu
+	open_menu(choose_team_menu)
 
 func _on_add_player_button_pressed() -> void:
 	number_of_players += 1
