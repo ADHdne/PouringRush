440122a1b43caa8faf719e9b7877c72ba6c33496
ADHdne <aadnevat@hotmail.com>
2026-06-28 19:11:03 +0200

    tweeking colors and volumes

--- pouring-rush/EndScene/end_scene.gd
+++ pouring-rush/EndScene/end_scene.gd
@@ -3,7 +3,8 @@ class_name EndScreen
 
 
 @onready var label: Label = $HBoxContainer/VBoxContainer/Label
-@onready var color_rect: ColorRect = $ColorRect
+@onready var color_rect: Panel = $ColorRect
+
 
 
 @export var winning_team : String
@@ -14,13 +15,19 @@ func _ready() -> void:
 	
 	label.text = winning_team
 	
+	var panel_stylebox = color_rect.get_theme_stylebox("panel") as StyleBoxTexture
+	
+	
+	# 3. Modify the texture modulate color
 	if winning_team == "Victory for the Red Team":
-		color_rect.color = Color(0.4, 0.1, 0.1)
+		panel_stylebox.modulate_color = Color(0.478, 0.157, 0.125, 0.62)
 	elif winning_team == "Victory for the Blue Team":
-		color_rect.color = Color(0.1, 0.1, 0.4)
+		panel_stylebox.modulate_color = Color(0.275, 0.306, 0.537, 0.62)
 	else:
-		color_rect.color = Color(0.2, 0.4, 0)
-
+		panel_stylebox.modulate_color = Color(0.227, 0.337, 0.0, 0.62)
+	
+	# 4. Re-apply the updated StyleBox back to the Panel's theme overrides
+	add_theme_stylebox_override("panel", panel_stylebox)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Start"):
